52b037fd880c5f2bc5ce5b375dbec925eac55f77
janexmgd <janexmgd@gmail.com>
2024-12-26 13:30:08 +0700

    updating route again lmao

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
-
+import 'dotenv/config';
 import cors from 'cors';
 import protectMiddleware from './middleware/protect.middleware.js';
 import errorHandler from './utils/errorHandler.js';
 import createError from './utils/createError.js';
 import selectService from './helpers/selectService.js';
 import { successResponse } from './helpers/response.js';
-
+const port = process.env.APP_PORT;
+if (!port) {
+  const err = new Error('set env APP_PORT ');
+  throw err;
+}
 const server = express();
 server.use(express.json());
 server.use(cors());
@@ -34,4 +38,6 @@ server.post('/service', async (req, res, next) => {
   }
 });
 server.use(errorHandler);
-export default server;
+server.listen(port, '0.0.0.0', () => {
+  console.log(`server running at port ${port}`);
+});
