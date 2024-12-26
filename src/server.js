74ad0f908e62a9fc42535a3e4390075955ed6a90
janexmgd <janexmgd@gmail.com>
2024-12-26 13:27:34 +0700

    updating route

--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
 import express from 'express';
-import 'dotenv/config';
+
 import cors from 'cors';
 import protectMiddleware from './middleware/protect.middleware.js';
 import errorHandler from './utils/errorHandler.js';
 import createError from './utils/createError.js';
 import selectService from './helpers/selectService.js';
 import { successResponse } from './helpers/response.js';
-const port = process.env.APP_PORT;
-if (!port) {
-  const err = new Error('set env APP_PORT ');
-  throw err;
-}
+
 const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(protectMiddleware);
 server.get('/', (req, res, next) => {
-  res
-    .json({
-      success: true,
-    })
-    .statusCode(200);
+  successResponse(res, {
+    code: 200,
+    message: 'ok',
+  });
 });
 server.post('/service', async (req, res, next) => {
   try {
@@ -39,6 +34,4 @@ server.post('/service', async (req, res, next) => {
   }
 });
 server.use(errorHandler);
-server.listen(port, '0.0.0.0', () => {
-  console.log(`server running at port ${port}`);
-});
+export default server;
